23818672858e00518c7008c02490846b9eea5876
Tom Hagander <tom.hagander@gmail.com>
2024-03-29 14:46:19 +0100

    committing

--- run_PACS.py
+++ run_PACS.py
@@ -32,32 +32,32 @@ commands = []
 
 #### 40 Clients ####
 # DAC with cosine similarity and inverse training loss, cosine origin and l2
-commands.append('python3 run_experiment.py --gpu 0 --dataset PACS --nbr_rounds 50 --nbr_clients 40 --seed 2 --batch_size 32 --nbr_local_epochs 1 --lr 7.5e-05 --stopping_rounds 30 --nbr_neighbors_sampled 2 --client_information_exchange DAC --similarity_metric cosine_similarity --measure_all_similarities True --experiment_name PACS_40_clients_DAC_cosine_initial_2')
-commands.append('python3 run_experiment.py --gpu 0 --dataset PACS --nbr_rounds 50 --nbr_clients 40 --seed 2 --batch_size 32 --nbr_local_epochs 1 --lr 7.5e-05 --stopping_rounds 30 --nbr_neighbors_sampled 2 --client_information_exchange DAC --similarity_metric inverse_training_loss --measure_all_similarities True --experiment_name PACS_40_clients_DAC_inv_loss_2')
-commands.append('python3 run_experiment.py --gpu 0 --dataset PACS --nbr_rounds 50 --nbr_clients 40 --seed 2 --batch_size 32 --nbr_local_epochs 1 --lr 7.5e-05 --stopping_rounds 30 --nbr_neighbors_sampled 2 --client_information_exchange DAC --similarity_metric cosine_origin --measure_all_similarities True --experiment_name PACS_40_clients_DAC_cosine_origin_2')
-commands.append('python3 run_experiment.py --gpu 0 --dataset PACS --nbr_rounds 50 --nbr_clients 40 --seed 2 --batch_size 32 --nbr_local_epochs 1 --lr 7.5e-05 --stopping_rounds 30 --nbr_neighbors_sampled 2 --client_information_exchange DAC --similarity_metric l2 --measure_all_similarities True --experiment_name PACS_40_clients_DAC_l2_2')
+commands.append('python3 run_experiment.py --gpu 0 --dataset PACS --nbr_rounds 50 --nbr_clients 40 --seed 3 --batch_size 32 --nbr_local_epochs 1 --lr 7.5e-05 --stopping_rounds 30 --nbr_neighbors_sampled 2 --client_information_exchange DAC --similarity_metric cosine_similarity --measure_all_similarities True --experiment_name PACS_40_clients_DAC_cosine_initial_3')
+commands.append('python3 run_experiment.py --gpu 0 --dataset PACS --nbr_rounds 50 --nbr_clients 40 --seed 3 --batch_size 32 --nbr_local_epochs 1 --lr 7.5e-05 --stopping_rounds 30 --nbr_neighbors_sampled 2 --client_information_exchange DAC --similarity_metric inverse_training_loss --measure_all_similarities True --experiment_name PACS_40_clients_DAC_inv_loss_3')
+commands.append('python3 run_experiment.py --gpu 0 --dataset PACS --nbr_rounds 50 --nbr_clients 40 --seed 3 --batch_size 32 --nbr_local_epochs 1 --lr 7.5e-05 --stopping_rounds 30 --nbr_neighbors_sampled 2 --client_information_exchange DAC --similarity_metric cosine_origin --measure_all_similarities True --experiment_name PACS_40_clients_DAC_cosine_origin_3')
+commands.append('python3 run_experiment.py --gpu 0 --dataset PACS --nbr_rounds 50 --nbr_clients 40 --seed 3 --batch_size 32 --nbr_local_epochs 1 --lr 7.5e-05 --stopping_rounds 30 --nbr_neighbors_sampled 2 --client_information_exchange DAC --similarity_metric l2 --measure_all_similarities True --experiment_name PACS_40_clients_DAC_l2_3')
 # testing for the previous four commands
-commands.append('python3 test_PACS.py --experiment PACS_40_clients_DAC_cosine_initial_2')
-commands.append('python3 test_PACS.py --experiment PACS_40_clients_DAC_inv_loss_2')
-commands.append('python3 test_PACS.py --experiment PACS_40_clients_DAC_cosine_origin_2')
-commands.append('python3 test_PACS.py --experiment PACS_40_clients_DAC_l2_2')
+commands.append('python3 test_PACS.py --experiment PACS_40_clients_DAC_cosine_initial_3')
+commands.append('python3 test_PACS.py --experiment PACS_40_clients_DAC_inv_loss_3')
+commands.append('python3 test_PACS.py --experiment PACS_40_clients_DAC_cosine_origin_3')
+commands.append('python3 test_PACS.py --experiment PACS_40_clients_DAC_l2_3')
 
 # Oracle and Random with inverse training loss
-commands.append('python3 run_experiment.py --gpu 0 --dataset PACS --nbr_rounds 50 --nbr_clients 40 --seed 2 --batch_size 32 --nbr_local_epochs 1 --lr 7.5e-05 --stopping_rounds 30 --nbr_neighbors_sampled 2 --client_information_exchange oracle --delusion -1 --similarity_metric inverse_training_loss --measure_all_similarities True --experiment_name PACS_40_clients_Random_2')
-commands.append('python3 run_experiment.py --gpu 0 --dataset PACS --nbr_rounds 50 --nbr_clients 40 --seed 2 --batch_size 32 --nbr_local_epochs 1 --lr 7.5e-05 --stopping_rounds 30 --nbr_neighbors_sampled 2 --client_information_exchange oracle --similarity_metric inverse_training_loss --measure_all_similarities True --experiment_name PACS_40_clients_Oracle_2')
+commands.append('python3 run_experiment.py --gpu 0 --dataset PACS --nbr_rounds 50 --nbr_clients 40 --seed 3 --batch_size 32 --nbr_local_epochs 1 --lr 7.5e-05 --stopping_rounds 30 --nbr_neighbors_sampled 2 --client_information_exchange oracle --delusion -1 --similarity_metric cosine_similarity --measure_all_similarities True --experiment_name PACS_40_clients_Random_3')
+commands.append('python3 run_experiment.py --gpu 0 --dataset PACS --nbr_rounds 50 --nbr_clients 40 --seed 3 --batch_size 32 --nbr_local_epochs 1 --lr 7.5e-05 --stopping_rounds 30 --nbr_neighbors_sampled 2 --client_information_exchange oracle --similarity_metric cosine_similarity --measure_all_similarities True --experiment_name PACS_40_clients_Oracle_3')
 # testing
-commands.append('python3 test_PACS.py --experiment PACS_40_clients_Random_2')
-commands.append('python3 test_PACS.py --experiment PACS_40_clients_Oracle_2')
+commands.append('python3 test_PACS.py --experiment PACS_40_clients_Random_3')
+commands.append('python3 test_PACS.py --experiment PACS_40_clients_Oracle_3')
 
 # no communication
-commands.append('python3 run_experiment.py --gpu 0 --dataset PACS --nbr_rounds 50 --nbr_clients 40 --seed 2 --batch_size 32 --nbr_local_epochs 1 --lr 7.5e-05 --stopping_rounds 30 --nbr_neighbors_sampled 2 --client_information_exchange no_exchange --similarity_metric inverse_training_loss --measure_all_similarities True --experiment_name PACS_40_clients_no_comm_2')
+commands.append('python3 run_experiment.py --gpu 0 --dataset PACS --nbr_rounds 50 --nbr_clients 40 --seed 3 --batch_size 32 --nbr_local_epochs 1 --lr 7.5e-05 --stopping_rounds 30 --nbr_neighbors_sampled 2 --client_information_exchange no_exchange --similarity_metric l2 --measure_all_similarities True --experiment_name PACS_40_clients_no_comm_3')
 # testing
-commands.append('python3 test_PACS.py --experiment PACS_40_clients_no_comm_2')
+commands.append('python3 test_PACS.py --experiment PACS_40_clients_no_comm_3')
 
 # all data
-commands.append('python3 run_experiment.py --gpu 0 --dataset PACS --nbr_rounds 50 --nbr_clients 4 --seed 2 --batch_size 32 --nbr_local_epochs 1 --lr 7.5e-05 --stopping_rounds 30 --nbr_neighbors_sampled 2 --client_information_exchange no_exchange --similarity_metric cosine_similarity --measure_all_similarities True --experiment_name PACS_40_clients_all_data_2')
-# TEMP testing
-commands.append('python3 test_PACS.py --experiment PACS_40_clients_all_data_2')
+commands.append('python3 run_experiment.py --gpu 0 --dataset PACS --nbr_rounds 50 --nbr_clients 4 --seed 3 --batch_size 32 --nbr_local_epochs 1 --lr 7.5e-05 --stopping_rounds 30 --nbr_neighbors_sampled 2 --client_information_exchange no_exchange --similarity_metric cosine_similarity --measure_all_similarities True --experiment_name PACS_40_clients_all_data_3')
+# testing
+commands.append('python3 test_PACS.py --experiment PACS_40_clients_all_data_3')
 
 
 for command in commands:
