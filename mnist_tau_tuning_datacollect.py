487ad4f072b03c71953f2af0ffe401c00b7facc5
Tom Hagander <tom.hagander@gmail.com>
2024-04-22 16:25:43 +0200

    did a lot of things ni

--- mnist_tau_tuning_datacollect.py
+++ mnist_tau_tuning_datacollect.py
@@ -14,14 +14,18 @@ mnist_trainingweight_cosine_expnames = ['fashion_MNIST_DAC_cosine_tau_100_seed_1
                                         'fashion_MNIST_DAC_cosine_tau_200_seed_1',
                                         'fashion_MNIST_DAC_cosine_tau_300_seed_1',
                                         'fashion_MNIST_DAC_cosine_tau_500_seed_1',
-                                        'fashion_MNIST_DAC_cosine_tau_2000_seed_1']
+                                        'fashion_MNIST_DAC_cosine_tau_2000_seed_1',
+                                        'fashion_MNIST_DAC_cosine_tau_5000_seed_1',
+                                        'fashion_MNIST_DAC_cosine_tau_10000_seed_1']
 
 mnist_trainingweight_origin_expnames = ['fashion_MNIST_DAC_cosine_origin_tau_50_seed_1',
                                         'fashion_MNIST_DAC_cosine_origin_tau_100_seed_1',
                                         'fashion_MNIST_DAC_cosine_origin_tau_200_seed_1',
                                         'fashion_MNIST_DAC_cosine_origin_tau_300_seed_1',
                                         'fashion_MNIST_DAC_cosine_origin_tau_500_seed_1',
-                                        'fashion_MNIST_DAC_cosine_origin_tau_2000_seed_1']
+                                        'fashion_MNIST_DAC_cosine_origin_tau_2000_seed_1',
+                                        'fashion_MNIST_DAC_cosine_origin_tau_5000_seed_1',
+                                        'fashion_MNIST_DAC_cosine_origin_tau_10000_seed_1']
 
 mnist_priorweight_invloss_expnames = ['fashion_MNIST_DAC_priorweight_invloss_tau_1_seed_1',
                                       'fashion_MNIST_DAC_priorweight_invloss_tau_5_seed_1',
---
+++ test_fashion_MNIST.py
@@ -0,0 +1,54 @@
+from utils.testing_utils import test_on_fashion_MNIST
+import argparse
+import pickle
+
+if __name__ == '__main__':
+
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--experiment', type=str, default='None', help='Experiment name')
+    parser.add_argument('--quick', type=bool, default=False, help='Quick test, not implemented')
+    parser.add_argument('--verbose', type=bool, default=True, help='Verbose output')
+    args = parser.parse_args()
+
+    quick = args.quick
+    verbose = args.verbose
+
+    metadata_path = 'save/' + args.experiment + '/metadata.txt'
+    # load metadata to dictionary
+    # Initialize an empty dictionary to store the file content
+    metadata = {}
+
+    # Open the file and read line by line
+    with open(metadata_path, 'r') as file:
+        for line in file:
+            # Strip white space and split by colon
+            key, value = line.strip().split(': ')
+            # Convert numerical values from strings
+            if value.replace('.', '', 1).isdigit():
+                if '.' in value:
+                    value = float(value)
+                else:
+                    value = int(value)
+            # Add to dictionary
+            metadata[key] = value
+
+    path = 'save/' + args.experiment + '/clients.pkl'
+
+    if verbose:
+        print('Testing fashion_MNIST on clients from experiment: {}'.format(args.experiment))
+        print('Loading clients from: {}'.format(path))
+
+    # import clients from clients.pkl
+    with open(path, 'rb') as f:
+        clients = pickle.load(f)
+    
+    if verbose:
+        print('Clients loaded')
+
+    acc_matrix = test_on_fashion_MNIST(clients, quick=quick, verbose=verbose)
+        
+    if verbose:
+        print('Testing complete')
+    
+    with open('save/'+args.experiment+'/fashion_MNIST_acc_matrix.pkl', 'wb') as f:
+        pickle.dump(acc_matrix, f)
