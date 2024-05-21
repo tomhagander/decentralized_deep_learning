a86e8048e1ab205941355f38e6de4c169151ddc9
Tom Hagander <tom.hagander@gmail.com>
2024-05-21 13:07:33 +0200

    sync

--- run_multiple_experiments.py
+++ run_multiple_experiments.py
@@ -1675,7 +1675,7 @@ toy_priorweight_cosine_origin_tauopt = 140
 
 lr = 0.003
 lr_invloss_l2 = 0.008
-
+'''
 # invloss trainingweight
 for seed in seeds:
     for trainset_size in trainset_sizes:
@@ -1723,7 +1723,9 @@ for seed in seeds:
     for trainset_size in trainset_sizes:
         expname = 'TOY_cosine_origin_priorweight_trainset_size_{}_seed_{}'.format(trainset_size, seed)
         commands.append('python3 run_experiment.py --gpu 0 --dataset toy_problem --nbr_rounds 40 --nbr_clients 99 --n_data_train {} --n_data_val 100 --seed {} --batch_size 8 --nbr_local_epochs 1 --lr {} --stopping_rounds 50 --nbr_neighbors_sampled 5 --prior_update_rule softmax --similarity_metric cosine_origin --tau {} --client_information_exchange DAC --experiment_name {} --aggregation_weighting priors'.format(trainset_size, seed, lr, toy_priorweight_cosine_origin_tauopt, expname))
+'''
 
+commands.append('python3 test_multiple.py')
 
 print('Commands to be run: ')
 for command in commands:
@@ -1734,7 +1736,7 @@ print('Running {} commands'.format(len(commands)))
 
 # timebomb sleep for 1 hour
 sleeptime = 0
-sleeptime = 60*60*0.001
+sleeptime = 60*60*12.5
 print('Sleeping for {} seconds'.format(sleeptime))
 time.sleep(sleeptime)
 
